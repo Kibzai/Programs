714809c7b567bb4ffa75022dc0dcd52c415545d9
Kibzai <Kibsaim.Mejia@protonmail.com>
2026-07-11 16:16:33 -0600

    Jekyll

---
+++ index.md
@@ -0,0 +1,10 @@
+---
+layout: default
+title: Inicio
+---
+
+# Bienvenido a mis programas
+
+Aquí encontrarás mis notas y scripts.
+
+[Ver README](README.md)
--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Protocolos: Setup & Checks
 start https://kibzai.github.io/Programs/
+
+
+# Install dependencies
+gem install bundler
+bundle install
+
+# Build and serve
+bundle exec jekyll serve

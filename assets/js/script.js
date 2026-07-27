1aeb97901df87983be333d504079c487848aed44
Kibzai <Kibsaim.Mejia@protonmail.com>
2026-07-27 00:52:31 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js")
       .then((reg) => console.log("✅ SW registrado:", reg))
       .catch((err) => console.error("❌ Error SW:", err));
   });
